777e593995da2dafdec8ae00e4e7c35f4a23a8f2
RSepulvedaDev10 <94391613+RSepulvedaDev10@users.noreply.github.com>
2022-01-04 10:17:52 -0600

    Update Dev10 M07 - Class Coding.py

--- Dev10 M07 - Class Coding.py
+++ Dev10 M07 - Class Coding.py
@@ -1,11 +1,11 @@
 class Animal:
-    def __init__(self, name, species, age):
+    def __init__(self, name, animalClass, weight):
         self.name = name
-        self.species = species
-        self.age = age
+        self.animalClass = animalClass
+        self.weight = weight
     
     def showDescription(self):
-        print(f"This animal is a {self.name}, a type of {self.species}, and is {self.age} year(s) old.")
+        print(f"This animal is a {self.name}, a type of {self.species}, and has a weight of {self.weight}")
     
     def getName(self):
         print(f"Getting name")
@@ -25,6 +25,44 @@ class Animal:
     def rest(self):
         print(f"Resting")
         print(f"{self.name} is now rested")
+        
+    def procreate(self):
+        print(f"Procreating")
+        print(f"There is now more of {self.name}")
+
+class Fish(Animal):
+    def __init__(self, name, animalClass, weight, species, freshWater):
+        super().__init__(name, animalClass, weight)
+        self.species = species
+        self.freshWater = freshWater
+        
+    def swim(self):
+        print(f"{self.name} is swimming in the water")
+
+class Snake(Animal):
+    def __init__(self, name, animalClass, weight, species, length, venomous):
+        super().__init__(name, animalClass, weight)
+        self.species = species
+        self.length = length
+        self.venomous = venomous
+    
+    def hibernate(self):
+        print(f"{self.name} is hibernating for the season")
+    
+    def slither(self):
+        print(f"{self.name} is slithering on the ground")
+
+class Person(Animal):
+    def __init__(self, name, animalClass, weight, age, ethnicity):
+        super().__init__(name, animalClass, weight)
+        self.age = age
+        self.ethnicity = ethnicity
+        
+    def sleep(self):
+        print(f"{self.name} has fallen asleep and won't be up for a few hours")
+        
+    def walk(self):
+        print(f"{self.name} is walking around")
 
 class Book:
     def __init__(self, title, author, pages, yearPublished):
@@ -53,16 +91,41 @@ class Book:
     def close(self):
         print(f"Closing '{self.title}'")
 
+class Textbook(Book):
+    def __init__(self, title, author, pages, yearPublished, ISBN, subject, publisher):
+        super().__init__(title, author, pages, yearPublished)
+        self.ISBN = ISBN
+        self.subject = subject
+        self.publisher = publisher
+        
+    def answerQuestion(self):
+        print(f"Answering questions from the textbook")
+
+class AddressBook(Book):
+    def __init__(self, title, author, pages, yearPublished, contactName, postalAddress, phoneNumber, notes):
+        super().__init__(title, author, pages, yearPublished)
+        self.contactName = contactName
+        self.postalAddress = postalAddress
+        self.phoneNumber = phoneNumber
+        self.notes = notes
+    
+    def enterInfo(self, contactName, postaladdress, phoneNumber, notes):
+        print(f"{self.contactName}, {self.postalAddress}, and {self.phoneNumber} have been added to the book")
+    
+    def deleteInfo(self, contactName, postaladdress, phoneNumber, notes):
+        print(f"{self.contactName}, {self.postalAddress}, and {self.phoneNumber} have been deleted from the book")
+ 
+
 class Vehicle:
-    def __init__(self, name, color, style, mileage, year):
+    def __init__(self, name, color, style, capacity, year):
         self.name = name
         self.color = color
         self.style = style
-        self.mileage = mileage
+        self.capacity = capacity
         self.year = year
         
     def showDescription(self):
-        print(f"This vehicle is a {self.name}, which is a {self.color} {self.style} made in {self.year}, and has a milage of {self.mileage}.")
+        print(f"This vehicle is a {self.name}, which is a {self.color} {self.style} made in {self.year}, and has a capacity of {self.capacity} seats.")
         
     def getName(self):
         print(f"Getting name")
@@ -75,8 +138,17 @@ class Vehicle:
     def setColor(self, color):
         print(f"Changing color")
         self.color = color
-    
-    def start(self):
+        
+    def move(self):
+        print(f"{self.name} is now in motion")
+        
+class Car(Vehicle):
+    def __init__(self, name, color, style, capacity, year, mileage, manufacturer):
+        super().__init__(name, color, style, capacity, year)
+        self.mileage = mileage
+        self.manufacturer = manufacturer
+        
+    def startEngine(self):
         print(f"{self.name} has been started")
         
     def drive(self):
@@ -85,33 +157,54 @@ class Vehicle:
     def brake(self):
         print(f"{self.name} has slowed down")
         
-    def stop(self):
+    def stopEngine(self):
         print(f"{self.name} has stopped")
 
+class Bicycle(Vehicle):
+    def __init__(self, name, color, style, capacity, year, material):
+        super().__init__(name, color, style, capacity, year)
+        self.material = material
+        
+    def pedal(self):
+        print(f"{self.name} is in motion")
+        
+    def brake(self):
+        print(f"{self.name} has slowed down and may have stopped")
 
-a = Animal ('dog', 'mammal', 12)
-b = Book ('Harry Potter and the Sorcerers Stone', 'JK Rowling', 223, '6/26/1997')
-v = Vehicle ('Chrysler 300', 'red', 'sedan', 125000, '1/1/2015')
+class Boat(Vehicle):
+    def __init__(self, name, color, style, capacity, year, type):
+        super().__init__(name, color, style, capacity, year)
+        self.type = type
+        
+    def startEngine(self):
+        print(f"{self.name} has been started")
+        
+    def drive(self):
+        print(f"{self.name} is now in motion")
+        
+    def brake(self):
+        print(f"{self.name} has slowed down")
+        
+    def stopEngine(self):
+        print(f"{self.name} has stopped")
 
-
-a.showDescription()
-a.getName()
-a.setName('Cat')
-a.move()
-a.eat()
-a.rest()
-b.showDescription()
-b.getTitle()
-b.setTitle('Harry Potter and the Chamber of Secrets')
-b.open()
-b.read()
-b.close()
-v.showDescription()
-v.getName()
-v.setName('Chrysler 200')
-v.setColor('blue')
-v.showDescription()
-v.start()
-v.drive()
-v.brake()
-v.stop()
+class HotAirBalloon(Vehicle):
+    def __init__(self, name, color, style, capacity, year, altitude):
+        super().__init__(name, color, style, capacity, year)
+        self.altitude = altitude
+    
+    def takeoff(self):
+        print(f"{self.name} has taken off the ground")
+        
+    def fly(self):
+        print(f"{self.name} is moving in the air")
+        
+    def increaseAltitude(self):
+        print(f"{self.name} has gone higher in the air")
+        
+    def decreaseAltitude(self):
+        print(f"{self.name} has lowered from the sky")
+        
+    def land(self):
+        print(f"{self.name} has landed on the ground")
+    
